78ae5a0bed119795316920633cf9e5baaceb3fea
Martin Dressler <m.dressler321@gmail.com>
2024-12-02 05:54:00 +0100

    Delete DRE0065_PROJECT directory

--- DRE0065_PROJECT/js/app.js
+++
@@ -1,5 +0,0 @@
-function toggleInfo() 
-{
-    var infos = document.getElementsByClassName('additional-info'); 
-    for(var i = 0; i < infos.length; i++) infos[i].style.display = infos[i].style.display === 'none' ? 'block' : 'none';
-}
