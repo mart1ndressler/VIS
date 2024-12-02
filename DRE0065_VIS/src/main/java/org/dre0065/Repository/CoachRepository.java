e078de54621d98eb2d0304173534165ea82deab3
Martin Dressler <m.dressler321@gmail.com>
2024-12-02 05:54:09 +0100

    Delete DRE0065_VIS directory

--- DRE0065_VIS/src/main/java/org/dre0065/Repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dre0065.Repository;
-
-import org.dre0065.Model.Coach;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CoachRepository extends JpaRepository<Coach, Integer> {
-}
